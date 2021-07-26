375449ef4dcc19437e73d00d0c45ce14150c2149
Ajay Upreti <ajayupreti@Ajays-MacBook-Air.local>
2021-07-26 19:39:11 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.cea8643adb6bca727e3c1fbc77395ae4.js"
+  "/portfolio/precache-manifest.b463af342a34bdab4bec3c7e63982629.js"
 );
 
 self.addEventListener('message', (event) => {
