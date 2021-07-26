7f267868cc3579ec064cf6b2e0beada207c9167a
ajayupreti <ajayupreti20194@gmail.com>
2021-07-26 14:33:42 +0000

    Deploying to gh-pages from  @ 1364aa1487be1be52a189bf42134f453b46f0593 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.4bd860e2438ab1a155ed0629fc2f3d00.js"
+  "/portfolio/precache-manifest.a51cd95109b18457755c2192644067be.js"
 );
 
 self.addEventListener('message', (event) => {
